4bda490081158803321cac0227f10f2895e838c0
Aizooh <isaackaris.52@gmail.com>
2024-02-05 03:01:37 +0300

    late

---
+++ script.js
@@ -0,0 +1,7 @@
+function togglemenu(){
+    const menu = document.querySelector(".menu_links")
+        const icon = document.querySelector(".icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+
+}

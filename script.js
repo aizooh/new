b85827f08e69e1903856ab610f22171485595402
Aizooh <isaackaris.52@gmail.com>
2024-02-09 20:34:04 +0300

    update
    updated

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function togglemenu(){
     icon.classList.toggle("open")
 
 }
+  // Add an event listener to check the scroll position
+  window.addEventListener('scroll', function() {
+    var button = document.getElementById('glowButton');
+    var scrollPosition = window.scrollY;
+
+    // Adjust the scroll position value based on your needs
+    if (scrollPosition >50) {
+      button.classList.add('glow');
+    } else {
+      button.classList.remove('glow');
+    }
+  });
